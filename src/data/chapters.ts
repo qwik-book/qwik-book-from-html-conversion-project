54d4fe2b45b5fb1ba58347b942f0447c9fc5239d
Anartz Mugika Ledo <mugan86@gmail.com>
2023-08-20 19:59:21 +0200

    feat(chapter-07-practice): accordion implement. wip refactor

---
+++ src/data/chapters.ts
@@ -0,0 +1,42 @@
+export const chaptersData = [
+    {
+        "question": "1.- Introducción a Qwik",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": true
+    },
+    {
+        "question": "2.- Enrutamiento",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "3.- Componentes",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "4.- Layouts / Plantillas",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "5.- Estilos",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "6.- SSR - Server Side Rendering",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "7.- Gestión de Estado",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    },
+    {
+        "question": "8.- Consumiendo APIs REST / GraphQL",
+        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "open": false
+    }
+];
