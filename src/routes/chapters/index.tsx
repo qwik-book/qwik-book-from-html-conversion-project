72d5fa2725624a5ec9c4927d264df56b97798e52
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-08-21 07:20:38 +0200

    fix: remove unused style reference

--- src/routes/chapters/index.tsx
+++ src/routes/chapters/index.tsx
@@ -1,12 +1,10 @@
 import { component$, useSignal, useStyles$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { chaptersData } from "~/data/chapters";
-import chaptersStyles from "./chapter.css?inline";
 import Accordion from "~/components/shared/accordion";
 
 export default component$(() => {
   const chapters = useSignal(chaptersData);
-  useStyles$(chaptersStyles);
   return (
     <>
       <div class="container mt-2">
