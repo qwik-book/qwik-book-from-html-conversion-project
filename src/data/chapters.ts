3f022ebcfceeab85ef1b3befff2f0fb689319689
anartzdev <105705996+anartzdev@users.noreply.github.com>
2024-01-12 06:30:08 +0100

    feat: update chapters content

--- src/data/chapters.ts
+++ src/data/chapters.ts
@@ -1,46 +1,3 @@
-/*export const chaptersData = [
-    {
-        "question": "1.- Introducción a Qwik",
-        "answer": "Introducción a Qwik y explicación de las características de Qwik y primer ejemplo básico donde vamos a trabajar creando varias rutas sencillas antes de comenzar a trabajar de manera más detallada en aspectos del framework",
-        "open": true
-    },
-    {
-        "question": "2.- Enrutamiento",
-        "answer": "Lo que vamos a ver en este capítulo es una continuación a lo visto anteriormente en lo que se refiere al sistema de enrutamiento (Routing) y su funcionamiento, usando Qwik City donde lo veremos con todo detalle, tratando todos los aspectos más importantes y necesarios para poder trabajar de diferentes formas con ello.",
-        "open": false
-    },
-    {
-        "question": "3.- Componentes",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    },
-    {
-        "question": "4.- Layouts / Plantillas",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    },
-    {
-        "question": "5.- Estilos",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    },
-    {
-        "question": "6.- SSR - Server Side Rendering",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    },
-    {
-        "question": "7.- Gestión de Estado",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    },
-    {
-        "question": "8.- Consumiendo APIs REST / GraphQL",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
-        "open": false
-    }
-];*/
-
 export const chaptersData = [
     {
         "question": "1.- Introducción a Qwik",
@@ -74,84 +31,84 @@ export const chaptersData = [
     },
     {
         "question": "7.- SSR - Server Side Rendering",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Comenzamos con un nuevo capítulo en el que vamos a hablar sobre `SSR` aplicado a Qwik. Antes de empezar con el capítulo y mostraros sus caracterísiticas y como es su funcionamiento, lo ideal es saber que es exactamente `SSR`.",
         "open": false
     },
     {
         "question": "8.- Gestión de Estado - I",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "En este capítulo comenzaremos dando los primeros pasos con las claves para entender como funciona el control de estados y realizaremos casos prácticos para afianzar los conocimientos teóricos.",
         "open": false
     },
     {
         "question": "9.- Gestión de Estado - II",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "En este caso vamos a aprender a hacer uso del hook `useComputed$()`, función que nos va a facilitar mucho el trabajo cuando trabajamos con valores computados, permitiendo hacer tracking (observar cambios) por defecto y encima de eso, devolver un valor como resultado.",
         "open": false
     },
     {
         "question": "10.- Gestión de Estado - III - Práctica adaptando HTML a Qwik",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Práctica orientada al proyecto real que estamos trabajando desde el capítulo 5, donde implementaremos mediante el uso de la gestión de estados el apartado de capítulos que estaba pendiente de implementarlo.",
         "open": false
     },
     {
         "question": "11.- Consumiendo APIs REST / GraphQL",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "El objetivo principal es que aprendamos a hacer uso de este recurso como es consumir una API en Qwik y por esa razón, os voy a proporcionar todas las claves para entender como podemos consumir información de APIs externas mediante casos prácticos.",
         "open": false
     },
     
     {
         "question": "12.- Ciclos de Vida",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Comenzamos con un nuevo capítulo en el que vamos a ver un concepto super importante y que bajo mi punto de vista es esencial para subir de nivel en cualquier tecnología como en este caso en Qwik Framework.",
         "open": false
     },
     
     {
         "question": "13.- Formularios",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Los formularios desempeñan un papel importante en el mundo de la programación web, y en particular en el desarrollo de aplicaciones basadas en Qwik y tecnologías similares como Angular, React, Vue, Astro,etc.",
         "open": false
     },
     {
         "question": "14.- Eventos",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Vamos a trabajar con los eventos, que son sucesos o acciones que ocurrirán en cualquier página web",
         "open": false
     },
     {
         "question": "15.- Content Projection",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Vamos a trabajar con un concepto avanzado que es el de proyectar información de un componente a otro, y esto lo hacemos mediante el uso de los Slots que nos permiten realizar la proyección de contenido",
         "open": false
     },
     {
         "question": "16.- Custom Hooks",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Vamos a sumergir en un concepto que bajo mi punto de vista es muy importante conocer y dominar. Vamos a explorar al detalle los custom hooks en Qwik, comprendiendo su estructura, su uso y los beneficios que aportan a nuestros proyectos.",
         "open": false
     },
     {
         "question": "17.- Testing",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Aprenderemos a testear nuestros componentes y proyectos de Qwik",
         "open": false
     },
     {
         "question": "18.- Library Mode",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Todas las claves para poder crear nuestra propia librería Qwik, paso a paso, y distribuirla en NPM para que otras personas puedan utilizarla, al igual que nosotros utilizamos librerías externas en nuestros proyectos",
         "open": false
     },
     {
         "question": "19.- Integraciones",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Claves para aprender a trabajar con las integraciones de librerías proporcionadas por Qwik con varias funcionalidades y servicios útiles, como la integración que nos permite trabajar con componentes React en Qwik que veremos en el capítulo 20.",
         "open": false
     },
     {
         "question": "20.- Integraciones React",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "En este capítulo os voy a enseñar a utilizar React dentro de Qwik utilizando `Qwik React` basándonos en los conocimientos iniciales que hemos adquirido sobre las integraciones que nos proporciona Qwik",
         "open": false
     },
     {
         "question": "21.- Deploy",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Claves para poder publicar nuestros proyectos en Internet y así mostrarlos al mundo",
         "open": false
     },
     {
         "question": "22.- Cheatsheet",
-        "answer": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ...",
+        "answer": "Recopilación de los aspectos más importantes a tener en cuenta para poder seguir trabajando con Qwik",
         "open": false
     }
 ];
